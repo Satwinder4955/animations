942c0917b0df7a5f6233927ce9e2cffcdaded5fb
Satwinder Singh <108409791+Satwinder4955@users.noreply.github.com>
2023-01-29 20:51:43 +0530

    engine trains.
    it's a sample of engine train site by which you can book engine trip if we had site on 1980's.

---
+++ webpage/form.js
@@ -0,0 +1,11 @@
+document.addEventListener("onclick", form);
+
+function form() {
+  var form = document.querySelector("button");
+  // console.log(form);
+  if (form.getElementById("example") === form.getElementById("example")) {
+    alert("search other location");
+  } else {
+    document.getElementById("example");
+  }
+}
